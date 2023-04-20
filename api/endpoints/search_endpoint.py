052efdec8c53a020c25228ce0a261ff4fce4e83e
Mark Kozlov <mark.k.2012@yandex.ru>
2023-04-20 12:53:42 +0300

    Add celery [not ideal]

--- api/endpoints/search_endpoint.py
+++ api/endpoints/search_endpoint.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter, Depends
 from pydantic import UUID4
 from sqlalchemy.orm import Session
+from celery.result import AsyncResult
 
 from config import settings
 from schemas.search_schema import SearchSettings, SearchId, SearchResult
 from db.connection import get_session
 from db.repositories.search_db import add_search_to_db, get_search_from_db
-from service.extract_text import check_text_from_files
+from service.extract_text import check_text_from_files, celery_app
+import redis
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
+
 
 router = APIRouter(tags=["main"], prefix="")
 
@@ -17,8 +22,19 @@ def search_settings(search_config: SearchSettings, session: Session = Depends(ge
     return {"search_id": search_id}
 
 
-@router.get("/searches/{search_id}", response_model=SearchResult)
+@router.get("/searches/{search_id}", response_model=SearchResult, response_model_exclude_none=True)
 def search_result(search_id: UUID4, session: Session = Depends(get_session)):
+    task_in_queue = redis_client.exists(str(search_id))
+    if task_in_queue:
+        print('Oieiei')
+        return {"finished": False}
+    
+    task = AsyncResult(str(search_id), app=celery_app)
+    if task.status == "SUCCESS":
+        return task.result
+    print(task.status)
     search_config = get_search_from_db(search_id=search_id, session=session)
     search_config = SearchSettings.from_orm(search_config)
-    return check_text_from_files(search_config, settings.TARGET_DIRECTORY)
+    
+    task = check_text_from_files.apply_async(args=[search_config.json(), settings.TARGET_DIRECTORY], task_id=str(search_id))
+    return {"finished": False}
--- service/extract_text.py
+++ service/extract_text.py
@@ -1,26 +1,30 @@
-# import os
-
-# import requests
-
-from schemas.search_schema import SearchSettings
-from utils.get_all_files import get_all_files
+import json
 
 import tika
 tika.initVM()
 from tika import parser
+from celery import Celery
+
+from schemas.search_schema import SearchSettings
+from utils.get_all_files import get_all_files
 
 
-def check_text_from_files(search_settings: SearchSettings, target_directory: str):
-    paths = []
+celery_app = Celery('tasks', broker='redis://localhost:6379/0', backend='redis://localhost:6379/1', task_track_started=True)
+# celery_app.conf.task_track_started = True
+
+@celery_app.task()
+def check_text_from_files(search_settings: str, target_directory: str):
+    """ Search text_template in files and return paths """
+    # self.update_state(state="PROGRESS")
+    
+    json_settings = json.loads(search_settings)
+    search_settings = SearchSettings(**json_settings)
+    
     file_paths = get_all_files(search_settings, target_directory)
-    # tika_url = f"http://tika-server:9998/tika"
-    # headers = {"Accept": "text/plain"}
     search_string = search_settings.text
-
+    
+    paths = []
     for path in file_paths:
-        # with open("path/to/your/file", "rb") as f:
-        #     file_data = f.read()
-        # response = requests.put(tika_url, data=path, headers=headers)
         path = str(path.absolute())
         parsed = parser.from_file(path)["content"]
         if parsed is not None and parsed.find(search_string) > -1:
